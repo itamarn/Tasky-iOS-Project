e06d8407f65a343d34868707c937db0877c073b6
Itamar Nakar <user@MacBook-Pro.local>
2014-12-17 09:29:34 +0200

    Initial Commit

---
+++ Tasky/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tasky
+//
+//  Created by Itamar Nakar on 17/12/14.
+//  Copyright (c) 2014 Itamar Nakar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
